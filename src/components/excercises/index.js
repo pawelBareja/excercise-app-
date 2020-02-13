233f2ba29248e1e0bec8ac7ba360fb0da401ea3b
pawel <bareja.pawel@gmail.com>
2020-02-13 20:37:08 +0100

    layout

--- src/components/excercises/index.js
+++ src/components/excercises/index.js
@@ -1,7 +1,25 @@
 import React from "react";
+import LeftPane from "./LeftPane";
+import RightPane from "./RightPane";
+import { Grid } from "@material-ui/core";
+
+const styles = {
+  Paper: {
+    padding: 20,
+    marginTop: 20,
+    marginBottom: 20
+  }
+};
 
 export default props => (
   <>
-    <p>excercise</p>
+    <Grid container>
+      <Grid item xs>
+        <LeftPane styles={styles} />
+      </Grid>
+      <Grid item xs>
+        <RightPane styles={styles} />
+      </Grid>
+    </Grid>
   </>
 );
--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { AppBar, Toolbar } from "@material-ui/core";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
 
 export default props => (
   <>
-    <AppBar>
-      <Toolbar></Toolbar>
+    <AppBar position="relative">
+      <Toolbar>
+        <Typography variant="overline" color="inherit">
+          Excercises App
+        </Typography>
+      </Toolbar>
     </AppBar>
   </>
 );
